2f871033ced85fa50d93d12ddedf822666274766
Era <30367131+EraGamer@users.noreply.github.com>
2019-01-16 14:04:02 -0600

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,1 +1,7 @@
+local list = {"1","2","3","4","5","6","7","8","9"}
 
+for i = 1,#list do
+  local Button = script.Parent.Container['Button'..tostring(list[i])]
+  Button.MouseButton1Click:Connect(function()    
+  end)
+end
